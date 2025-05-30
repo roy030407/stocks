7fca3f0ef72a1062ed90cb163ef8dfb5f4820ef6
roy030407 <101629448+roy030407@users.noreply.github.com>
2025-05-30 21:50:21 +0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import StockDashboard from '../frontend/StockDashboard'
+import StockDashboard from '@frontend/StockDashboard'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <StockDashboard />
   </React.StrictMode>,
-) 
+)
